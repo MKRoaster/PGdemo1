18f7f9ad0edd847d43c5282524a3ec1315656573
Piotr Grzywacz <piotr.grzywacz@assecods.pl>
2022-07-07 15:16:50 +0200

    First project upload

---
+++ src/main/java/com/example/pgdemo1/PGdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.pgdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PGdemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PGdemo1Application.class, args);
+    }
+
+}
